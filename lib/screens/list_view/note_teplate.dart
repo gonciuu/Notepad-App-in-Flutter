8ae665dbd3e980806e07dc901cd86c636a0a6bea
andryue <kacperwojak17@gmail.com>
2020-08-07 22:05:22 +0200

    create sample note listview template

--- lib/screens/list_view/note_teplate.dart
+++ lib/screens/list_view/note_teplate.dart
@@ -1,14 +1,50 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NoteTemplate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.only(top:8.0,bottom: 8.0),
-        child: ListTile(),
-      ),
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+          child: ListTile(
+            title: Text(
+              "Note Sample Title",
+              style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20.0,
+                  color: Colors.grey[900]),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 5.0),
+              child: Text(
+                "Note Sample Subtitle here",
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 17.0,
+                    color: Colors.grey[500]),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            trailing: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.arrow_back,
+                  color: Colors.blueGrey,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Divider(
+          color: Colors.grey[600],
+        )
+      ],
     );
   }
 }
