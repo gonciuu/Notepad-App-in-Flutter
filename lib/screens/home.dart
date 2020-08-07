1cd413ec3e2401bfcf6a66817acc025abe010c18
andryue <kacperwojak17@gmail.com>
2020-08-07 21:40:40 +0200

    create home screen template

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,7 +10,37 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("JD"),)
+     body: SafeArea(
+       child: Padding(
+         padding: const EdgeInsets.only(top:18.0,left: 12.0,right: 12.0),
+         child: Column(
+           children: <Widget>[
+             Row(
+               mainAxisAlignment:MainAxisAlignment.spaceBetween,
+               children: <Widget>[
+                 IconButton(
+                   icon: Icon(Icons.menu, color: Colors.grey[900],), onPressed: () {},color: Colors.red[400],iconSize: 32.0,
+                 ),
+                 Text(
+                   "My Notes",style: TextStyle(
+                   fontSize: 28.0,
+                   fontWeight: FontWeight.w700,
+                   color: Colors.grey[900]
+                 ),
+                 ),
+                 IconButton(
+                   icon: Icon(Icons.search, color: Colors.grey[900],), onPressed: () {},color: Colors.red[400],iconSize: 32.0,
+                 ),
+               ],
+             ),
+             SizedBox(height: 30.0,),
+             ListView(
+               
+             )
+           ],
+         ),
+       ),
+     ),
     );
   }
 }
