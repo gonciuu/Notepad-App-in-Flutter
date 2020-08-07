0dcb340bc9427edb1f59e75984f386f53e3a31d1
andryue <kacperwojak17@gmail.com>
2020-08-07 21:52:18 +0200

    create listview

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
-
 import 'package:flutter/material.dart';
+
+import 'list_view/note_teplate.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,37 +11,57 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: SafeArea(
-       child: Padding(
-         padding: const EdgeInsets.only(top:18.0,left: 12.0,right: 12.0),
-         child: Column(
-           children: <Widget>[
-             Row(
-               mainAxisAlignment:MainAxisAlignment.spaceBetween,
-               children: <Widget>[
-                 IconButton(
-                   icon: Icon(Icons.menu, color: Colors.grey[900],), onPressed: () {},color: Colors.red[400],iconSize: 32.0,
-                 ),
-                 Text(
-                   "My Notes",style: TextStyle(
-                   fontSize: 28.0,
-                   fontWeight: FontWeight.w700,
-                   color: Colors.grey[900]
-                 ),
-                 ),
-                 IconButton(
-                   icon: Icon(Icons.search, color: Colors.grey[900],), onPressed: () {},color: Colors.red[400],iconSize: 32.0,
-                 ),
-               ],
-             ),
-             SizedBox(height: 30.0,),
-             ListView(
-               
-             )
-           ],
-         ),
-       ),
-     ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 18.0, left: 12.0, right: 12.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(
+                        Icons.menu,
+                        color: Colors.grey[900],
+                      ),
+                      onPressed: () {},
+                      color: Colors.red[400],
+                      iconSize: 32.0,
+                    ),
+                    Text(
+                      "My Notes",
+                      style: TextStyle(
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.grey[900]),
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.search,
+                        color: Colors.grey[900],
+                      ),
+                      onPressed: () {},
+                      color: Colors.red[400],
+                      iconSize: 32.0,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                ListView.builder(
+                  itemBuilder: (BuildContext context, int index) => NoteTemplate(),
+                  itemCount: 25,
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/screens/list_view/note_teplate.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class NoteTemplate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.only(top:8.0,bottom: 8.0),
+        child: ListTile(),
+      ),
+    );
+  }
+}
