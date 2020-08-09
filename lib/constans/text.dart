864795d3f3a0ece9e729dd1c6bad5b26d4e100c8
andryue <kacperwojak17@gmail.com>
2020-08-09 09:46:44 +0200

    finish layout ui

--- lib/constans/text.dart
+++ lib/constans/text.dart
@@ -13,3 +13,5 @@ const textFieldDecoration = InputDecoration(
     enabledBorder: OutlineInputBorder(borderSide: BorderSide(color:Color.fromARGB(120, 15, 34, 102),width: 1.5)),
     focusedBorder: OutlineInputBorder(borderSide: BorderSide(color:Color.fromARGB(255, 15, 34, 102),width: 1.5))
 );
+
+
--- lib/screens/write_note.dart
+++ lib/screens/write_note.dart
@@ -10,9 +10,10 @@ class _WriteNoteState extends State<WriteNote> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Padding(
-            padding: const EdgeInsets.fromLTRB(35.0, 50.0, 35.0, 30.0),
+            padding: const EdgeInsets.fromLTRB(35.0, 50.0, 35.0, 35.0),
             child: Form(
                 child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -21,8 +22,10 @@ class _WriteNoteState extends State<WriteNote> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     Text("Add New Note",
-                        style:
-                            TextStyle(color: Colors.grey[900], fontSize: 28.0)),
+                        style: TextStyle(
+                            color: Colors.grey[900],
+                            fontSize: 28.0,
+                            fontWeight: FontWeight.w700)),
                     IconButton(
                       icon: Icon(
                         Icons.clear,
@@ -37,35 +40,110 @@ class _WriteNoteState extends State<WriteNote> {
                   height: 60.0,
                 ),
                 TextFormField(
-                  decoration: textFieldDecoration,//title
+                  decoration: textFieldDecoration, //title
                 ),
                 SizedBox(
                   height: 20.0,
                 ),
                 TextFormField(
                   decoration: textFieldDecoration.copyWith(
-                    hintText: "Write Here note description",//description
-                    labelText: "Description"
-                  ),
+                      hintText: "Write Here note description", //description
+                      labelText: "Description"),
                 ),
                 SizedBox(
                   height: 20.0,
                 ),
-                TextFormField(
-                  decoration: textFieldDecoration.copyWith(
-                      hintText: "Write Here note description",//description
-                      labelText: "Description"
-                  ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextFormField(
+                        decoration: textFieldDecoration.copyWith(
+                            hintText: "Write Here note date", //date
+                            labelText: "Date"),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 25.0,
+                    ),
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Color.fromARGB(255, 15, 34, 102),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.calendar_today,
+                          color: Colors.white,
+                          size: 30.0,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(
                   height: 20.0,
                 ),
-                TextFormField(
-                  decoration: textFieldDecoration.copyWith(
-                      hintText: "Write Here note description",//description
-                      labelText: "Description"
-                  ),
-                ),SizedBox(height: 60.0,),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextFormField(
+                        decoration: textFieldDecoration.copyWith(
+                            hintText: "Write Here note time", //date
+                            labelText: "Time"),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 25.0,
+                    ),
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Color.fromARGB(255, 15, 34, 102),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.timer,
+                          color: Colors.white,
+                          size: 30.0,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: FlatButton(
+                        padding: EdgeInsets.symmetric(vertical: 15.0),
+                        color: Color.fromARGB(255, 15, 34, 102),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)
+                        ), onPressed: () {  }, child: Text("Home",style: TextStyle(color: Colors.white),),
+                      ),
+                    ),SizedBox(width: 20.0,),
+                    Expanded(
+                      child: FlatButton(
+                        padding: EdgeInsets.symmetric(vertical: 15.0),
+                        color: Colors.grey[200],
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)
+                        ), onPressed: () {  }, child: Text("Business"),
+                      ),
+                    ),SizedBox(width: 20.0,),Expanded(
+
+                      child: FlatButton(
+                        padding: EdgeInsets.symmetric(vertical: 15.0),
+                        color: Colors.grey[200],
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)
+                        ), onPressed: () {  }, child: Text("Other"),
+                      ),
+                    )
+
+                  ],
+                ),
+                Spacer(),
                 Row(
                   children: <Widget>[
                     Expanded(
