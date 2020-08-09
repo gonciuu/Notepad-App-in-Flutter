43d9987333a32f9f37614dcfaa09a81374cb77dc
andryue <kacperwojak17@gmail.com>
2020-08-09 09:31:40 +0200

    add inputs and input decoradions to screen

---
+++ lib/constans/text.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+const descriptionTxt =  TextStyle(color: Colors.grey, fontSize: 14.0);
+
+
+const textFieldDecoration = InputDecoration(
+    hintText: "Write Here note title",
+    labelText:"Title",
+    labelStyle: TextStyle(
+        color: Color.fromARGB(255, 15, 34, 102)
+    ),
+    enabledBorder: OutlineInputBorder(borderSide: BorderSide(color:Color.fromARGB(120, 15, 34, 102),width: 1.5)),
+    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color:Color.fromARGB(255, 15, 34, 102),width: 1.5))
+);
--- lib/screens/write_note.dart
+++ lib/screens/write_note.dart
@@ -1,3 +1,4 @@
+import 'package:enotepad/constans/text.dart';
 import 'package:flutter/material.dart';
 
 class WriteNote extends StatefulWidget {
@@ -19,19 +20,67 @@ class _WriteNoteState extends State<WriteNote> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Text("Add New Note",style: TextStyle(color: Colors.grey[900],fontSize: 28.0)),
+                    Text("Add New Note",
+                        style:
+                            TextStyle(color: Colors.grey[900], fontSize: 28.0)),
                     IconButton(
-                      icon: Icon(Icons.clear,size: 32.0, color: Colors.grey[400],),
-                      onPressed: () {
-
-                      },
+                      icon: Icon(
+                        Icons.clear,
+                        size: 32.0,
+                        color: Colors.grey[400],
+                      ),
+                      onPressed: () {},
                     )
                   ],
                 ),
-                FlatButton(
-                  onPressed: () {  },
-                  color: Colors.,
-                  child: Text("Add Note"),
+                SizedBox(
+                  height: 60.0,
+                ),
+                TextFormField(
+                  decoration: textFieldDecoration,//title
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  decoration: textFieldDecoration.copyWith(
+                    hintText: "Write Here note description",//description
+                    labelText: "Description"
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  decoration: textFieldDecoration.copyWith(
+                      hintText: "Write Here note description",//description
+                      labelText: "Description"
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  decoration: textFieldDecoration.copyWith(
+                      hintText: "Write Here note description",//description
+                      labelText: "Description"
+                  ),
+                ),SizedBox(height: 60.0,),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: FlatButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        padding: EdgeInsets.symmetric(vertical: 19.0),
+                        onPressed: () {},
+                        color: Color.fromARGB(255, 15, 34, 102),
+                        child: Text("Add Note",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 22.0)),
+                      ),
+                    ),
+                  ],
                 )
               ],
             ))),
