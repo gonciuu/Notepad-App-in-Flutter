2e679d43178c823c5b730af4f7d028188541eb93
andryue <kacperwojak17@gmail.com>
2020-08-11 09:50:20 +0200

    create note class and constructor for this

---
+++ lib/models/note.dart
@@ -0,0 +1,18 @@
+class Note {
+  String title, description, time, date, category;
+  int isStar, id;
+
+  Note({this.id,
+        this.description,
+        this.category,
+        this.title,
+        this.date,
+        this.isStar,
+        this.time});
+
+
+
+
+
+
+}
